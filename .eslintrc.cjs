78c17ea23ce637f50bd2cf1df5c4cb2badcf5887
Justin Linn <justin@linnjs.com>
2024-02-24 14:21:17 -0500

    feat(home-page): Add home page layout

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,8 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
