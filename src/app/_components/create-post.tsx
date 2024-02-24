561746bda2b70b408957a65360e5a7b37cb3f4e7
Justin Linn <justin@linnjs.com>
2024-02-24 12:45:58 -0500

    feat(ui): add schadcn-ui

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { api } from "@/trpc/react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export function CreatePost() {
   const router = useRouter();
@@ -24,20 +26,20 @@ export function CreatePost() {
       }}
       className="flex flex-col gap-2"
     >
-      <input
+      <Input
         type="text"
         placeholder="Title"
         value={name}
         onChange={(e) => setName(e.target.value)}
         className="w-full rounded-full px-4 py-2 text-black"
       />
-      <button
+      <Button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
         disabled={createPost.isLoading}
       >
         {createPost.isLoading ? "Submitting..." : "Submit"}
-      </button>
+      </Button>
     </form>
   );
 }
