5a098d2866f6b1f734a4565c1e105966eaebb289
Justin Linn <justin@linnjs.com>
2024-02-25 08:03:51 -0500

    feat(routes): Add redirects on protected routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,39 +2,54 @@
 
 import { Background } from 'src/components/ui/background';
 
-import { getDraftTasks } from '@/app/dashboard/actions';
+import { getDraftTasks, getIssueTasks } from '@/app/dashboard/actions';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { getServerAuthSession } from '@/server/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await getServerAuthSession();
-  console.log('session: ', session);
 
   if (!session) {
     redirect('/');
   }
 
   const draftItems = await getDraftTasks();
-  console.log('draftItems: ', draftItems);
+  const issueItems = await getIssueTasks();
 
   return (
     <main className="mx-auto min-h-screen">
-      <div className="flex items-center justify-center">
+      <div className="flex h-40 items-center justify-center">
         <Background />
       </div>
 
-      <div className="space-y-4">
-        {draftItems?.map((item) => {
-          return (
-            <Card key={item.id}>
-              <CardHeader>{item.title}</CardHeader>
-              <CardContent>{item.body}</CardContent>
-              <CardFooter></CardFooter>
-            </Card>
-          );
-        })}
-      </div>
+      <section className="space-y-8 py-8">
+        <div className="space-y-4">
+          <h2 className="text-lg font-bold">All Drafts</h2>
+          {draftItems?.map((item) => {
+            return (
+              <Card key={item.id}>
+                <CardHeader>{item.title}</CardHeader>
+                <CardContent>{item.body}</CardContent>
+                <CardFooter></CardFooter>
+              </Card>
+            );
+          })}
+        </div>
+
+        <div className="space-y-4">
+          <h2 className="text-lg font-bold">All Issues</h2>
+          {issueItems?.map((item) => {
+            return (
+              <Card key={item.id}>
+                <CardHeader>{item.title}</CardHeader>
+                <CardContent>{item.body}</CardContent>
+                <CardFooter></CardFooter>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
     </main>
   );
 }
