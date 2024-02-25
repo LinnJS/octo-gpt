23cb1541add72301d3eef5c5e6468ff920d96ea7
Max Braun <maxbraun175@yahoo.com>
2024-02-25 12:33:45 -0500

    Add draft issue page by ID (#12)
    * add draft issue page by ID
    
    * fix

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -4,19 +4,21 @@ import { getServerAuthSession } from '@/server/auth';
 import { redirect } from 'next/navigation';
 import { getDraftTaskById } from './action';
 
-export default async function Home() {
+export default async function Home({ params }: { params: { id: string } }) {
   const session = await getServerAuthSession();
 
   if (!session) {
     redirect('/');
   }
 
-  const draftIssue = await getDraftTaskById('DI_lAHOAWpDi84AdSIqzgFen6E');
+  const draftIssue = await getDraftTaskById(params.id);
   console.log('draftIssue: ', draftIssue);
 
   return (
     <main className="mx-auto min-h-screen">
-      <h1>hello</h1>
+      <h1 className='text-2xl font-semibold'>{draftIssue.title}</h1>
+      <p className='text-sm text-gray-600'>Updated at: {draftIssue.updatedAt}</p>
+      <p>{draftIssue.body}</p>
     </main>
   );
 }
--- src/app/dashboard/_components/dashboard-layout.tsx
+++ src/app/dashboard/_components/dashboard-layout.tsx
@@ -102,6 +102,7 @@ export function DashboardLayout({ children, draftTasks }: { children: React.Reac
                               return (
                                 <li key={task.id}>
                                   <a
+                                    href={"/dashboard/" + task.id}
                                     className={cn(
                                       false
                                         ? 'bg-gray-800 text-white'
@@ -154,6 +155,7 @@ export function DashboardLayout({ children, draftTasks }: { children: React.Reac
                       return (
                         <li key={task.id}>
                           <a
+                          href={"/dashboard/" + task.id}
                             className={cn(
                               false ? 'bg-gray-800 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white',
                               'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
