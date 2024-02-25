43433ee6e3a3940485e608fbe4c1fd1a0b628cb3
Maxwell Braun <maxbraun175@yahoo.com>
2024-02-25 12:49:15 -0500

    add user name to nav

---
+++ src/app/dashboard/[id]/_components/generate-button.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+
+export default function GenerateButton({ id }: { id: string }){
+
+  function generate(){
+    console.log("Generate body");
+  }
+
+  return (
+    <Button className="mt-5" onClick={generate}>Generate Body</Button>
+  )
+}
--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -3,6 +3,8 @@
 import { getServerAuthSession } from '@/server/auth';
 import { redirect } from 'next/navigation';
 import { getDraftTaskById } from './action';
+import { Button } from '@/components/ui/button';
+import GenerateButton from './_components/generate-button';
 
 export default async function Home({ params }: { params: { id: string } }) {
   const session = await getServerAuthSession();
@@ -19,6 +21,7 @@ export default async function Home({ params }: { params: { id: string } }) {
       <h1 className='text-2xl font-semibold'>{draftIssue.title}</h1>
       <p className='text-sm text-gray-600'>Updated at: {draftIssue.updatedAt}</p>
       <p>{draftIssue.body}</p>
+      <GenerateButton id={params.id}/>
     </main>
   );
 }
